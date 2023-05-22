e6600825050a824627946d5ac5c3f300b41f8d8e
Joao Guilherme <joao.guilherme100@hotmail.com>
2023-05-22 10:13:14 -0300

    Criacao da Model

---
+++ ContactManagement/Models/Contact.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ContactManagement.Models
+{
+    [Table("Contact")]
+    public class Contact
+    {
+        [Column("Id")]
+        [Display(Name = "ID")]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [Column("Name")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column("Contact")]
+        [Display(Name = "Contact")]
+        [RegularExpression(@"^\d{9}$", ErrorMessage = "Contact must be 9 digits.")]
+        public string ContactNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Column("E-mail")]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+    }
+}
---
+++ ContactManagement/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ContactManagement.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contact { get; set; }
+    }
+}
